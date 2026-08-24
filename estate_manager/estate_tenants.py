069dc24eab3199f1ba6eb6049c59a751850b4ed4
nnamdi-security <peter.chizobam@xultech.ng>
2026-08-24 20:16:23 +0100

    first commit

---
+++ estate_manager/estate_tenants.py
@@ -0,0 +1,11 @@
+new_tenants = {
+    [
+        "tenants":{"name":"Peter", "phone":"812-3456-789"}
+    ]
+}
+
+def tenants():
+    """
+    This function asks for the new tenant's first and last name and append it to the net tenants dictionary record
+    """
+    
