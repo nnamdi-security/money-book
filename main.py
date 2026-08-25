2583364c812668180b6c1b0456f5658c68ec8298
nnamdi-security <peter.chizobam@xultech.ng>
2026-08-25 07:44:52 +0100

    feat: add function to view payment history of estate members

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from estate_manager.data_storage import load_data
 from estate_manager.estate_tenants import show_all_members, register_new_member
-from estate_manager.payment_record import record_payment
+from estate_manager.payment_record import record_payment, show_payment_history
 
 
 data = load_data()
-record_payment(data)
+#record_payment(data)
 #register_new_member(data)
 #show_all_members(data)
 
+show_payment_history(data)
